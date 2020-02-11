cd59b612569bdfed5aac5b5aaf3f83763dc904d9
ttslee <theodol2@uci.edu>
2020-02-10 23:45:20 -0800

    updated

---
+++ Assets/Loser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoserScript: MonoBehaviour
+{
+    public AudioClip loser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.Instance.Play(loser);
+    }
+    
+}
---
+++ Assets/Scripts/LoserScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loser : MonoBehaviour
+{
+    public AudioClip sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.Instance.Play(sound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
